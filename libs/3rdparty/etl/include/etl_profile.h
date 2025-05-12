ccadd3826aa063c8df56749cf891e38773f62103
Filipe Cuim <filipe.cuim@ctw.bmwgroup.com>
2025-05-12 14:27:51 +0100

    Update etl_profile to allow etl to deduce options automatically

--- libs/3rdparty/etl/include/etl_profile.h
+++ libs/3rdparty/etl/include/etl_profile.h
@@ -1,28 +1,14 @@
 #ifndef ETL_PROFILE_H
 #define ETL_PROFILE_H
 
-//*****************************************************************************
-// Generic C++14
-//*****************************************************************************
-
 #define ETL_TARGET_DEVICE_GENERIC
 #define ETL_TARGET_OS_NONE
-#define ETL_COMPILER_GCC
-#define ETL_CPP11_SUPPORTED 1
-#define ETL_CPP14_SUPPORTED 1
 
-#if __cplusplus >= 201703L
-#define ETL_CPP17_SUPPORTED 1
-#else
-#define ETL_CPP17_SUPPORTED 0
-#endif
-
-// workaround instead of 202002L because of old arm cross compiler version
-#if __cplusplus > 201703L
-#define ETL_CPP20_SUPPORTED 1
-#else
-#define ETL_CPP20_SUPPORTED 0
-#endif
+//*****************************************************************************
+// C++23 support is still not deduced by ETL, since there is no full support
+// by all compilers yet. So, if C++23 is to be used, we define here ETL's
+// necessary macro to enable its features.
+//*****************************************************************************
 
 #if __cplusplus >= 202302L
 #define ETL_CPP23_SUPPORTED 1
@@ -30,10 +16,7 @@
 #define ETL_CPP23_SUPPORTED 0
 #endif
 
-#define ETL_NO_NULLPTR_SUPPORT 0
-#define ETL_NO_LARGE_CHAR_SUPPORT 0
-#define ETL_CPP11_TYPE_TRAITS_IS_TRIVIAL_SUPPORTED 0
 #define ETL_NO_STL
-#define ETL_NO_INITIALIZER_LIST
+#define ETL_FORCE_STD_INITIALIZER_LIST
 
 #endif
